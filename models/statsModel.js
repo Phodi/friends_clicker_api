0deba4fc549ed79688d25f9a18de7be4ae028a2a
Phodi <5670253+Phodi@users.noreply.github.com>
2020-04-10 23:04:43 +0700

    change user clickRate default value

--- models/statsModel.js
+++ models/statsModel.js
@@ -2,13 +2,13 @@ const connection = require("../database_connection/mongooseConnection")
 
 const statsSchema = new connection.Schema({
   currentScore: { type: Number, required: true, default: 0 },
-  clickRate: { type: Number, required: true, default: 0 },
+  clickRate: { type: Number, required: true, default: 1 },
   autoRate: { type: Number, required: true, default: 0 },
   updated: { type: Date, required: true, default: Date.now },
-  user: { type: connection.Schema.Types.ObjectId, ref: "User" }
+  user: { type: connection.Schema.Types.ObjectId, ref: "User" },
 })
 
-statsSchema.methods.calculate = async function() {
+statsSchema.methods.calculate = async function () {
   const stats = this
 
   const secondsPassed = (Date.now() - stats.updated) / 1000
@@ -26,7 +26,7 @@ statsSchema.methods.calculate = async function() {
 
 statsSchema.statics.calculateAll = async () => {
   const cursor = Stats.find().cursor()
-  cursor.eachAsync(async stats => await stats.calculate())
+  cursor.eachAsync(async (stats) => await stats.calculate())
 }
 
 const Stats = connection.model("Stats", statsSchema)
